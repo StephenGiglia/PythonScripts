81d15b628b3d9c0d57b99bd38d7ac985616ed89f
StephenGiglia <40441591+StephenGiglia@users.noreply.github.com>
2020-01-05 00:15:09 -0500

    Created a recursive Fibonacci number calculator

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(number):
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        return(fibonacci(number - 1) + fibonacci(number - 2))
+
+
+
+num_iter = 25
+print(fibonacci(num_iter))
